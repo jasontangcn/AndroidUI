74d7c8e5b0cc456c60057ea889798b8275682427
jastang <TomHornson@hotmail.com>
2019-11-03 17:00:57 +0800

    code refactoring.

--- app/src/main/java/com/hyper/uidemo/fragment/ViewPagerFragmentAdapter.java
+++ app/src/main/java/com/hyper/uidemo/fragment/ViewPagerFragmentAdapter.java
@@ -8,10 +8,12 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public class ViewPagerFragmentAdapter extends FragmentPagerAdapter {
 	private ArrayList<Fragment> mFragments;
+
 	public ViewPagerFragmentAdapter(FragmentManager fm, ArrayList<Fragment> fragment) {
 		super(fm);
 		this.mFragments = fragment;
 	}
+
 	@Override
 	public int getCount() {
 		return mFragments.size();
